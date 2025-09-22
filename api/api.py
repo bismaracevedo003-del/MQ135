6884934f03a46fb0384a1ef0781f7d8d98c910ca
Bismar-Ac <bismaracevedo003@gmail.com>
2025-09-22 09:46:53 -0600

    CORS

--- api/api.py
+++ api/api.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 from datetime import datetime
 
 app = Flask(__name__)
+CORS(app)
 
 # Cambiamos de pyodbc a pymssql
 app.config['SQLALCHEMY_DATABASE_URI'] = (
